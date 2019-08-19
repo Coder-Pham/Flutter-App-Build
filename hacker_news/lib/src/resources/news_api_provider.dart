2b1bbcd39ba2a2dc588db1881c2668796cc6d0f0
Phạm Trọng Nhân <tendaigom@gmail.com>
2019-08-19 20:44:24 +0700

    [Fix] Method return type

--- hacker_news/lib/src/resources/news_api_provider.dart
+++ hacker_news/lib/src/resources/news_api_provider.dart
@@ -1,13 +1,14 @@
 import 'package:http/http.dart' show Client;
 import 'dart:convert';
 import '../model/item_model.dart';
+import 'dart:async';
 
 final String _apiurl = 'https://hacker-news.firebaseio.com/v0';
 
 class NewsApiProvider {
   Client client = Client();
 
-  fetchTopids() async {
+  Future<List<int>> fetchTopids() async {
     final response =
         await client.get(_apiurl + '/topstories.json?print=pretty');
     final ids = json.decode(response.body);
@@ -15,7 +16,7 @@ class NewsApiProvider {
     return ids;
   }
 
-  fetchItem(int id) async {
+  Future<ItemModel> fetchItem(int id) async {
     final response = await client.get(_apiurl + '/item/$id.json');
     return ItemModel.fromJson(json.decode(response.body));
   }
